82e68a76d2bf40338853b75ce20fc8127e004daa
Bohdan <l0mobot50@gmail.com>
2022-04-15 17:03:04 +0300

    edit SumMulti

--- src/chapter1/SumMulti.java
+++ src/chapter1/SumMulti.java
@@ -6,16 +6,9 @@ public class SumMulti {
     public void printNumbers(int[] number) {
         System.out.println();
         System.out.println("Sum : " + Arrays.stream(number).sum());
-
+        System.out.println("Multi: "+ Arrays.stream(number).reduce(1, (a, b) -> a * b));
     }
 
- //   private static int Multi(int[] number) {
- //       int mult;
- //       for (int i = 0; i < number.length-1; i++) {
- //
-//
- //      }
-//
- //       return mult;
-   }
+
+}
 
