b8db2dbed907bbf54299576d087581f9e4fa818a
Bohdan <l0mobot50@gmail.com>
2022-04-26 22:28:15 +0300

    add chapter3

--- src/chapter2/Runner.java
+++ src/chapter2/Runner.java
@@ -1,11 +1,7 @@
 package chapter2;
 
 import chapter2.varianta.DifferentDigitsNumberPrinter;
-import chapter2.varianta.MediumLargeDivider;
-import chapter2.varianta.ShortLongNumberPrinter;
-import chapter2.variavtb.MountPrinter;
-import chapter2.variavtb.MountPrinter2;
-import chapter2.variavtb.SquareRootCalculator;
+
 
 import static chapter2.helper.UserInputScanner.getStringInput;
 
@@ -14,37 +10,38 @@ public class Runner {
         String[] array = getStringInput();
 
 
-        //1 Вывести на консоль те числа, длина которых меньше (больше) средней,
-        //а также длину.
-        MediumLargeDivider mediumLarge = new MediumLargeDivider();
-        mediumLarge.printNumber(array);
-
-        //2 Найти самое короткое и самое длинное число. Вывести найденные числа
-        //и их длину.
-
-        ShortLongNumberPrinter shortLongNumberPrinter = new ShortLongNumberPrinter();
-        shortLongNumberPrinter.printNumber(array);
-
-        //3 Ввести число от 1 до 12. Вывести на консоль название месяца, соответству-
-        //ющего данному числу. Осуществить проверку корректности ввода чисел.
-        MountPrinter mountPrinter = new MountPrinter();
-        mountPrinter.printMount();
-
-        MountPrinter2 mountPrinter2 = new MountPrinter2();
-        mountPrinter2.printMount();
-
-
-        //4 Найти корни квадратного уравнения. Параметры уравнения передавать
-        //с командной строкой.
-
-        SquareRootCalculator squareRootCalculator = new SquareRootCalculator();
-        squareRootCalculator.printRoots();
-
-        //5Найти число, состоящее только из различных цифр. Если таких чисел не-
-        //сколько, найти первое из них.
+//        //1 Вывести на консоль те числа, длина которых меньше (больше) средней,
+//        //а также длину.
+//        MediumLargeDivider mediumLarge = new MediumLargeDivider();
+//        mediumLarge.printNumber(array);
+//
+//        //2 Найти самое короткое и самое длинное число. Вывести найденные числа
+//        //и их длину.
+//
+//        ShortLongNumberPrinter shortLongNumberPrinter = new ShortLongNumberPrinter();
+//        shortLongNumberPrinter.printNumber(array);
+//
+//        //3 Ввести число от 1 до 12. Вывести на консоль название месяца, соответству-
+//        //ющего данному числу. Осуществить проверку корректности ввода чисел.
+//        MountPrinter mountPrinter = new MountPrinter();
+//        mountPrinter.printMount();
+//
+//        MountPrinter2 mountPrinter2 = new MountPrinter2();
+//        mountPrinter2.printMount();
+//
+//
+//        //4 Найти корни квадратного уравнения. Параметры уравнения передавать
+//        //с командной строкой.
+//
+//        SquareRootCalculator squareRootCalculator = new SquareRootCalculator();
+//        squareRootCalculator.printRoots();
+//
+//        //5Найти число, состоящее только из различных цифр. Если таких чисел не-
+//        //сколько, найти первое из них.
 
         DifferentDigitsNumberPrinter differentNumbersPrinter = new DifferentDigitsNumberPrinter();
         differentNumbersPrinter.printNumber(array);
+
 
     }
 
--- src/chapter2/varianta/DifferentDigitsNumberPrinter.java
+++ src/chapter2/varianta/DifferentDigitsNumberPrinter.java
@@ -6,11 +6,15 @@ public class DifferentDigitsNumberPrinter {
     public void printNumber (String [] input){
         ArrayList<String> number = new ArrayList<>();
         for (String num : input){
-            if(isNumber(num)) number.add(num);
+            if(isNumber(Integer.parseInt(num))) number.add(num);
         }
-        System.out.println(number.get(0));
+        if(number.isEmpty()){
+            System.out.println("not number");;
+        } else {
+            System.out.println(number.get(0));
+        }
     }
-    public static boolean isNumber(String numbers) {
+    private static boolean isNumber(int numbers) {
         String s = String.valueOf(numbers);
         for (int i = 0; i < s.length(); i++) {
             for (int j = i + 1; j < s.length(); j++) {
