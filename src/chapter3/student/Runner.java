e953709a6be19578e9c20ec8351bed1f0bb4801f
Bohdan <l0mobot50@gmail.com>
2022-04-30 19:40:10 +0300

    edit chapter3

--- src/chapter3/student/Runner.java
+++ src/chapter3/student/Runner.java
@@ -3,20 +3,17 @@ package chapter3.student;
 
 public class Runner {
     public static void main(String[] args) {
-        Student st1 = new Student(1,"Ivanov","Ivan","Jovanovich", 23,7,2002,
-                "Kyiv",67_450_29_22,"Math",2,"MT1");
-        Student st2 = new Student(2,"Petrov","Petr","Petrovich",10,8,2008,
-                "Kyiv",67_123_22_21,"Philosophy",3,"PH2");
-        Student st3 = new Student(3,"Andreev","Andrei","Andreevich",12,4,2003,
-                "Kyiv",67_474_22_89,"Math",1,"MT1");
+        Student student = new Student();
+
         StudentList students = new StudentList();
-        students.addStudent(st1);
-        students.addStudent(st2);
-        students.addStudent(st3);
+        students.addStudent(student.st1);
+        students.addStudent(student.st2);
+        students.addStudent(student.st3);
 
 
         students.facultyPrint("Math");
-        students.facultyAndCoursePrint("Math",2);
+        students.facultyMathCourse2Print();
+        students.facultyPhilosophyCourse1Print();
         students.birthdayStudentPrint(2000);
         students.groupStudentPrint("MT1");
     }
--- src/chapter3/student/StudentList.java
+++ src/chapter3/student/StudentList.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentList {
-    public List<Student> studentsList = new ArrayList<>();
+    public List<StudentConstructor> studentsList = new ArrayList<>();
 
-    public void addStudent(Student s) {
+    public void addStudent(StudentConstructor s) {
         studentsList.add(s);
     }
 
     //a) список студентов заданного факультета;
     public void facultyPrint(String faculty) {
         System.out.println("List of students faculty : " + faculty);
-        for (Student st : studentsList) {
+        for (StudentConstructor st : studentsList) {
             if (st.getFaculty().equals(faculty)) {
                 System.out.println(st);
             }
@@ -21,10 +21,18 @@ public class StudentList {
     }
 
     //b) списки студентов для каждого факультета и курса;
-    public void facultyAndCoursePrint(String faculty, int course) {
-        System.out.println("List of students faculty: " + faculty + "  and course :" + course);
-        for (Student st : studentsList) {
-            if (st.getFaculty().equals(faculty) && st.getCourse() == course) {
+    public void facultyMathCourse2Print() {
+        System.out.println("List of students faculty Math course 2: " );
+        for (StudentConstructor st : studentsList) {
+            if (st.getFaculty().equals("Math") && st.getCourse() == 2) {
+                System.out.println(st);
+            }
+        }
+    }
+    public void facultyPhilosophyCourse1Print() {
+        System.out.println("List of students faculty Philosophy course 1: " );
+        for (StudentConstructor st : studentsList) {
+            if (st.getFaculty().equals("Philosophy") && st.getCourse() == 1) {
                 System.out.println(st);
             }
         }
@@ -33,7 +41,7 @@ public class StudentList {
     //c) список студентов, родившихся после заданного года;
     public void birthdayStudentPrint(int yearOfBirth) {
         System.out.println("List of student over " + yearOfBirth + " year of birth");
-        for (Student st : studentsList) {
+        for (StudentConstructor st : studentsList) {
             if (st.getYearOfBirth()>yearOfBirth) {
                 System.out.println(st);
             }
@@ -42,7 +50,7 @@ public class StudentList {
     //d) список учебной группы.
     public void  groupStudentPrint (String group){
         System.out.println("List of student group: " + group );
-        for (Student st : studentsList){
+        for (StudentConstructor st : studentsList){
             if(st.getGroup().equals(group)){
                 System.out.println(st);
             }
